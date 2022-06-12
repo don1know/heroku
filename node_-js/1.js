9d181fa07ddac0ae4a7dd2c2115278ada79b4e9c
zayn <mdrahul6967@gmail.com>
2022-06-12 13:10:28 +0600

    okkk

--- node_-js/1.js
+++ node_-js/1.js
@@ -1,7 +1,8 @@
 
 const express =require('express');
 const app =  express();
-app.set('view engine','ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.get('/',(req,res)=>{
     res.render('ok1')
 })
